fe779df1f1e214a56e81cb90c1e24cf275e90288
Vipin22875 <vipiningle01@gmail.com>
2025-01-26 18:02:56 +0530

    Commit-4 updated socials and fixed fontcolors

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,39 +1,39 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { Card, CardBody, CardHeader } from 'reactstrap'
-
+import { SOCIAL_PLATOFORM } from './constants/portfolioConstants'
+import profileImage from '../image/profile-image.jpg'
 const Profile = () => {
     return (
         <React.Fragment>
-            <div className='d-flex align-items-center justify-content-center'>
-                <Card>
+            <div className='d-flex align-items-center justify-content-center mt-5'>
+                <Card className='me-4'>
                     <div className='fw-600 text-bold ms-3 fs-45 portfolio-name'>
                         Vipin Ingle
                     </div>
-                    <CardBody>
-                        Hi 👋, I'm <span className='portfolio-name'>Vipin. </span> 
-                        Software Engineer -
-                        <a href='https://ikshalabs.com/' target='_blank' className='portfolio-company-name fw-600'>
-                            Iksha Labs
+                    <CardBody className='fs-18'>
+                        Hi 👋, I'm <span className='portfolio-name'>Vipin. </span>
+                        Software Engineer at
+                        <a href='https://ikshalabs.com/' target='_blank' className='ms-1 portfolio-company-name fw-600'>
+                            Iksha Labs.
                         </a>
                         <p>
-                            Building tech with 
-                            <span className='react-color fw-800'> React</span>,
-                            <span className='python-color fw-800'> Python</span>,
-                            <span className='ruby-color fw-800'> Ruby on Rails </span>and
-                            <span className='sql-color fw-800'> SQL</span>.
+                            Passionate frontend developer with a robust foundation 
+                            in computer science and a keen eye for detail.
                         </p>
                         <p>
                             Connect with me on these platforms.
                         </p>
-                        <div className='w-50 d-flex justify-content-between'>
-                            <a href=''><i class="ci ci-github ci-2x"></i></a>
-                            <a href=''><i class="ci ci-linkedin ci-2x"></i></a>
-                            <a href=''><i class="ci ci-gmail ci-2x"></i></a>
-                            <a href=''><i class="ci ci-spotify ci-2x"></i></a>
+                        <div className='w-40 d-flex justify-content-between'>
+                            {
+                                SOCIAL_PLATOFORM.map((platform) => {
+                                    return <a href={platform.HREF} target='_blank'><i class={`ci ci-${platform.ICON_NAME} ci-2x`}></i></a>
+                                })
+                            }
                         </div>
-                        
                     </CardBody>
+                </Card>
+                <Card>
+                    <img src={profileImage} alt='' className='profile-image-container' />
                 </Card>
             </div>
         </React.Fragment>
---
+++ src/pages/constants/portfolioConstants.js
@@ -0,0 +1,15 @@
+export const SOCIAL_PLATOFORM = [
+    {
+        HREF: 'https://www.linkedin.com/in/vipin-ingle/',
+        ICON_NAME: 'linkedin'
+    },
+    {
+        HREF: 'https://github.com/Vipin22875',
+        ICON_NAME: 'github'
+    },
+    {
+        HREF: 'mailto:vipiningle01@gmail.com',
+        ICON_NAME: 'gmail'
+    },
+]
+
